707824076678794155f2a7c803bbcb069d899347
m1ner79 <michal.d.gornicki@gmail.com>
2020-06-24 11:06:01 +0100

    add test folder

---
+++ test/slack_mock/index.js
@@ -0,0 +1,12 @@
+const fetch = require('node-fetch');
+
+(async () => {
+	const body = {a: 1};
+	const response = await fetch('https://en1x1r494oruq.x.pipedream.net/', {
+		method: 'post',
+		body: JSON.stringify(body),
+		headers: {'Content-Type': 'application/json'}
+	});
+	const json = await response.json();
+	console.log(json);
+})();
